d5e7e93a108c1c94ab30a2f1f698217657f0e6a4
martin butler <martin.fullstack@gmail.com>
2015-02-16 09:15:15 -0500

    testing animation

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('vimeoMartinApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngFx'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
